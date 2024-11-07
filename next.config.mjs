344403b77f838f56d9fa96e7d42634718c8a9d47
nihadm97 <nihadm97@outlook.com>
2024-11-07 01:03:45 +0100

    Second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: "/:path*{.mp4}", // Matches all .mp4 files in the public directory
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "public, max-age=31536000, immutable", // Cache for 1 year
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
