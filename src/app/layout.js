dad0edc94548bbe9905d3e3517af9798d813d944
nihadm97 <nihadm97@outlook.com>
2024-11-08 22:34:55 +0100

    Second commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
