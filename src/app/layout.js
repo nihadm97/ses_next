6ad1b998181062f28cbc055236c1eca1c7414bd5
nihadm97 <nihadm97@outlook.com>
2024-11-09 11:21:51 +0100

    Second commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <SpeedInsights sampleRate={0.1}/>
+        <SpeedInsights sampleRate={0.5}/>
       </body>
     </html>
   );
