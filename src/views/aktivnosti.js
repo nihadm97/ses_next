5f2f85714f50caac19a97056cf09793a2daa643c
nihadm97 <nihadm97@outlook.com>
2024-12-06 22:08:20 +0100

    Second commit

--- src/views/aktivnosti.js
+++ src/views/aktivnosti.js
@@ -925,6 +925,9 @@ import sas4 from '@/views/images/sas4.jpg';
 import sas5 from '@/views/images/sas5.jpg';
 import sas6 from '@/views/images/sas6.jpg';
 
+import romania from '@/views/images/romania.jpg';
+
+
 const Aktivnosti = () => {
   useEffect(() => {
     AOS.init({duration: 2000});
@@ -944,6 +947,60 @@ const Aktivnosti = () => {
   return (
     <>
     <Menu fixed/>
+    <section data-aos="fade-in" className="pb-20 bg-white w-full">
+          <div className="container mx-auto px-4">
+            <span className="text-sm block my-4 p-3 text-blueGray-700 rounded border border-solid border-blueGray-100">
+              <h6 className="text-xl mb-1 font-semibold text-center uppercase">Emir Honić osvojio bronzanu medalju na Balkanskom prvenstvu Oradea 2024, Romania
+
+
+
+
+
+</h6>
+            </span>
+            <div className="flex flex-wrap">
+              <div className="lg:pt-12 pt-6 w-full px-6 text-center md:w-6/12">
+                <div className="relative flex flex-col min-w-0 break-words bg-blueGray-200 w-full mb-8 shadow-lg">
+                  <div className="py-5 flex-auto">
+                  <Carousel showThumbs={false} interval="10000" transitionTime="1000" infiniteLoop>
+                      
+                  <div>
+                        <Image src={romania} alt="" />
+                    </div>
+                   
+                    
+
+                    
+                   
+                    
+                </Carousel>
+                    </div>
+                  </div>
+                </div>
+              <div className="lg:pt-12 pt-6 w-full px-6 text-center md:w-6/12">
+                <div className="relative flex flex-col min-w-0 break-words bg-blueGray-200 w-full mb-8 shadow-">
+                  <div className="px-4 py-5 flex-auto">
+                  <span className="text-sm my-4 p-3 text-blueGray-700 rounded  border-solid border-blueGray-100">
+ 
+                  <p className="mb-4 text-blueGray-500">
+                  Na Balkanskom taekwondo prvenstvu, Oradea 2024 u Rumuniji,  održanom od 26. do 29. novembra, učenik I6 razreda Emir Honić osvojio bronzanu medalju! </p>
+                   <p className="mb-4 text-blueGray-500">
+                   Nastupivši  u kategoriji juniora do 63 kg, u izuzetno jakoj konkurenciji, osvojio je treće mjesto i potvrdio svoj kvalitet na regionalnoj sceni borilačkih sportova. Njegov fantastičan nastup obilježile su atraktivne borbe.</p>
+<p className="mb-4 text-blueGray-500">
+Ovaj izuzetan sportski događaj okupio je oko 476 takmičara iz 11 zemalja. Za reprezentaciju Bosne i Hercegovine nastupilo je  86 takmičara u svim dobnim kategorijama. U ekipnom plasmanu, reprezentacija BiH se pozicionirala na trece mjesto. U kategoriji  seniora/ki zauzeli smo drugo mjesto, a u kategoriji juniora/ki i kadeta/kinja treće mjesto.</p>
+<p className="mb-4 text-blueGray-500">
+Emir Honić je član kluba Olimpic. Klub Olimpic  je predstavljalo pet takmičara koji su osvojili tri bronzane medalje.</p>
+<p className="mb-4 text-blueGray-500">
+Sve čestitke našem Emiru za postignute rezultate! Ponosni smo na našeg Emira!</p>
+
+</span>
+                  </div>
+                 
+                </div>
+              </div>
+            </div>
+            </div>
+          </section>
     <section data-aos="fade-in" className="pb-20 bg-white w-full">
           <div className="container mx-auto px-4">
             <span className="text-sm block my-4 p-3 text-blueGray-700 rounded border border-solid border-blueGray-100">
