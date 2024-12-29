82f5f0b799757680db6f208e07fb7eeb6fde0673
nihadm97 <nihadm97@outlook.com>
2024-12-29 13:58:53 +0100

    Second commit

--- src/views/aktivnosti.js
+++ src/views/aktivnosti.js
@@ -970,6 +970,10 @@ import rod3 from '@/views/images/rod3.jpg';
 import rod4 from '@/views/images/rod4.jpg';
 import rod5 from '@/views/images/rod5.jpg';
 
+import bom1 from '@/views/images/bom1.jpg';
+import bom2 from '@/views/images/bom2.jpg';
+import bom3 from '@/views/images/bom3.jpg';
+
 const Aktivnosti = () => {
   useEffect(() => {
     AOS.init({duration: 2000});
@@ -1063,7 +1067,7 @@ Hvala našim dragim učenicama i učenicima što su svojim trudom i pažnjom ulj
           <section data-aos="fade-in" className="pb-20 bg-white w-full">
           <div className="container mx-auto px-4">
             <span className="text-sm block my-4 p-3 text-blueGray-700 rounded border border-solid border-blueGray-100">
-              <h6 className="text-xl mb-1 font-semibold text-center uppercase">Predavanje za roditelje
+              <h6 className="text-xl mb-1 font-semibold text-center uppercase"><b>Blagodati roditeljstva</b>
 
 
 
@@ -1088,6 +1092,69 @@ Hvala našim dragim učenicama i učenicima što su svojim trudom i pažnjom ulj
                     </div>
                     <div>
                         <Image src={rod5} alt="" />
+                    </div>
+                    
+                    
+
+                    
+                   
+                    
+                </Carousel>
+                    </div>
+                  </div>
+                </div>
+              <div className="lg:pt-12 pt-6 w-full px-6 text-center md:w-6/12">
+                <div className="relative flex flex-col min-w-0 break-words bg-blueGray-200 w-full mb-8 shadow-">
+                  <div className="px-4 py-5 flex-auto">
+                  <span className="text-sm my-4 p-3 text-blueGray-700 rounded  border-solid border-blueGray-100">
+ 
+                  <p className="mb-4 text-blueGray-500">
+                  U okviru programa <b>Blagodati roditeljstva</b> podržanog od strane Ministarstva za odgoj i obrazovanje i Instituta za razvoj preduniverzitetskog obrazovanja stručna služba naše škole je pripremila radionicu za roditelje <b>Kako graditi mostove razumijevanja</b>. Radionica je trajala 90 minuta, a cilj radionice je bio osnažiti roditelje za bolju komunikaciju sa adolescentima kroz vježbe, praktične savjete i razmjenu iskustava.</p>                   <p className="mb-4 text-blueGray-500">
+                   Cilj predavanja je bio da se roditelji upoznaju o ozbiljnosti navedene pojave, te o zakonskim aktima koja tretiraju čin lažnih dojavljivanja, kao i o krivičnoj odgovornosti, kako djece, tako i roditelja.</p>
+<p className="mb-4 text-blueGray-500">
+Roditelji su radionicu okarakterisali kao vrlo korisnu i priliku da steknu konkretne vještine koje mogu odmah primijeniti u svakodnevnom životu.
+</p>
+<p className="mb-4 text-blueGray-500">
+Neke od pozitivnih misli koje su proizašle iz radionice su:</p>
+<p className="mb-4 text-blueGray-500">
+Roditeljstvo je umjetnost puštanja, ali sposobnost postavljanja granica</p>
+<p className="mb-4 text-blueGray-500">
+Uspješno roditeljstvo nije savršenstvo nego trud da budete tu za svoje dijete</p>
+<p className="mb-4 text-blueGray-500">
+Najvažnija stvar koju možete dati djeci su korijeni i krila. Korijeni da znaju ko su i gdje uvijek mogu da se vrate, a krila da slobodno polete u svijet svjesni svojih mogućnosti i svoje vrijednosti.</p>
+</span>
+                  </div>
+                 
+                </div>
+              </div>
+            </div>
+            </div>
+          </section>
+          <section data-aos="fade-in" className="pb-20 bg-white w-full">
+          <div className="container mx-auto px-4">
+            <span className="text-sm block my-4 p-3 text-blueGray-700 rounded border border-solid border-blueGray-100">
+              <h6 className="text-xl mb-1 font-semibold text-center uppercase">Predavanje za roditelje
+
+
+
+
+
+</h6>
+            </span>
+            <div className="flex flex-wrap">
+              <div className="lg:pt-12 pt-6 w-full px-6 text-center md:w-6/12">
+                <div className="relative flex flex-col min-w-0 break-words bg-blueGray-200 w-full mb-8 shadow-lg">
+                  <div className="py-5 flex-auto">
+                  <Carousel showThumbs={false} interval="10000" transitionTime="1000" infiniteLoop>
+                      
+                  <div>
+                        <Image src={bom1} alt="" />
+                    </div>
+                    <div>
+                        <Image src={bom2} alt="" />
+                    </div>
+                    <div>
+                        <Image src={bom3} alt="" />
                     </div>
                     
                     
