30f590708dd0fd535daf69997a1cf3e47f8f6919
nihadm97 <nihadm97@outlook.com>
2026-03-22 18:46:25 +0100

    Novo

--- src/views/aktivnosti.js
+++ src/views/aktivnosti.js
@@ -1789,6 +1789,13 @@ import pinky5 from '@/views/images/pinky5.jpg';
 
 import sretannezavisnosti from '@/views/images/sretannezavisnosti.jpg';
 
+import down1 from '@/views/images/down1.jpg';
+import down2 from '@/views/images/down2.jpg';
+import down3 from '@/views/images/down3.jpg';
+import down4 from '@/views/images/down4.jpg';
+import down5 from '@/views/images/down5.jpg';
+import down6 from '@/views/images/down6.jpg';
+
 const Aktivnosti = () => {
   useEffect(() => {
     AOS.init({duration: 2000});
@@ -1808,6 +1815,82 @@ const Aktivnosti = () => {
   return (
     <>
     <Menu fixed/>
+    <section data-aos="fade-in" className="pb-20 bg-white w-full">
+          <div className="container mx-auto px-4">
+            <span className="text-sm block my-4 p-3 text-blueGray-700 rounded border border-solid border-blueGray-100">
+              <h6 className="text-xl mb-1 font-semibold text-center uppercase">𝑶𝒃𝒊𝒍𝒋𝒆𝒛̌𝒂𝒗𝒂𝒏𝒋𝒆 𝑫𝒂𝒏𝒂 𝒐𝒔𝒐𝒃𝒂 𝒔𝒂 𝑫𝒐𝒘𝒏𝒐𝒗𝒊𝒎 𝒔𝒊𝒏𝒅𝒓𝒐𝒎𝒐𝒎
+
+
+
+
+
+
+
+</h6>
+            </span>
+            <div className="flex flex-wrap">
+              <div className="lg:pt-12 pt-6 w-full px-6 text-center md:w-6/12">
+                <div className="relative flex flex-col min-w-0 break-words bg-blueGray-200 w-full mb-8 shadow-lg">
+                  <div className="py-5 flex-auto">
+                  <Carousel showThumbs={false} interval="10000" transitionTime="1000" infiniteLoop>
+                      
+                  <div>
+                        <Image src={down1} alt="" />
+                    </div> 
+                    <div>
+                        <Image src={down2} alt="" />
+                    </div> 
+                    <div>
+                        <Image src={down3} alt="" />
+                    </div> 
+                    <div>
+                        <Image src={down4} alt="" />
+                    </div> 
+                    <div>
+                        <Image src={down5} alt="" />
+                    </div> 
+                    <div>
+                        <Image src={down6} alt="" />
+                    </div> 
+
+                    
+                </Carousel>
+                    </div>
+                  </div>
+                </div>
+              <div className="lg:pt-12 pt-6 w-full px-6 text-center md:w-6/12">
+                <div className="relative flex flex-col min-w-0 break-words bg-blueGray-200 w-full mb-8 shadow-">
+                  <div className="px-4 py-5 flex-auto">
+                  <span className="text-sm my-4 p-3 text-blueGray-700 rounded  border-solid border-blueGray-100">
+ 
+                  
+
+<p className="mb-4 text-blueGray-500">
+U našoj školi obilježen je Dan osoba sa Downovim sindromom – dan posvećen ljubavi, prihvatanju i jednakosti.
+</p>
+<p className="mb-4 text-blueGray-500">
+Učenici/e i nastavnici/e su kroz različite aktivnosti pokazali koliko je važno razumijevanje i podrška osobama sa Downovim sindromom. Nosili smo šarene čarape kao simbol različitosti i podsjetnik da je upravo ta različitost ono što nas čini posebnima.
+</p>
+<p className="mb-4 text-blueGray-500">
+Kroz radionice, razgovore i kreativne aktivnosti, učili smo o važnosti inkluzije, prijateljstva i međusobnog poštovanja. Naši učenici/ce su još jednom pokazali veliko srce i spremnost da grade društvo bez predrasuda.
+</p>
+<p className="mb-4 text-blueGray-500">
+𝑷𝒐𝒓𝒖𝒌𝒂 𝒋𝒆 𝒋𝒂𝒔𝒏𝒂:
+𝑹𝒂𝒛𝒍𝒊𝒄̌𝒊𝒕𝒊 𝒔𝒎𝒐, 𝒂𝒍𝒊 𝒋𝒆𝒅𝒏𝒂𝒌𝒐 𝒗𝒓𝒊𝒋𝒆𝒅𝒏𝒊!
+</p>
+
+
+
+
+
+</span>
+                  </div>
+                 
+                </div>
+              </div>
+            </div>
+            </div>
+          </section>
     <section data-aos="fade-in" className="pb-20 bg-white w-full">
           <div className="container mx-auto px-4">
             <span className="text-sm block my-4 p-3 text-blueGray-700 rounded border border-solid border-blueGray-100">
